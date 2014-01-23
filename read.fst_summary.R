00e01ad816481c7e93cc5eed9d8954a2485b60f0
Alicia Mastretta-Yanes <A.Yanes@uea.ac.uk>
2014-01-23 12:30:59 +0000

    added function to read fat summary output file batch_1.fst_summary

---
+++ read.fst_summary.R
@@ -0,0 +1,12 @@
+read.fst_summary <- function(file, popNames){
+    ### Function to read the Stacks populations output file batch_1.fst_summary.tsv and add PopNames to it
+    # file = path to batch_1.fst_summary.tsv file
+    # popNames = vector with population names in the same order than PopID in the file 
+    
+    ### Get data
+    Fstmat<-data.matrix(read.delim(file = file, row.names=1, fill=TRUE)) 
+    # add col names
+    colnames(Fstmat)<- popNames
+    as.table(Fstmat)    
+  }
+  
